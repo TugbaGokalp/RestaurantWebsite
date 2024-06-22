e64f01d1bacbf551ad80099e46c119d058c21229
tugba.gokalp <tugbagokalp7265@gmail.com>
2024-06-22 21:06:00 +0300

    Added the ability to open and close the searchbar with toggle and close it when clicked out

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+// opening and closing the searchbar
 
+const searchForm = document.querySelector(".search-form");
+const searchBtn = document.querySelector("#searchBtn");
+
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(searchBtn) &
+      !e.composedPath().includes(searchForm)
+    ) {
+      searchForm.classList.remove("active");
+    }
+  });
+});
